10ac0fd81a57f8ba8a5249d6e7ddc8dacd42338b
KamuNHabuK <deminator21@gmail.com>
2021-11-29 23:34:40 +0300

    first commit

---
+++ script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import math
+
+a=float(input("Storona a = "))
+
+b=float(input("Storona b = "))
+
+s=a*b;
+
+SQFT_PER_ACRE=s/43.56;
+
+print((SQFT_PER_ACRE));
---
+++ script1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import math;
+
+NS=9.8;
+
+VI=0;
+
+d=float(input("Vysota d =  "));
+
+a=NS;
+
+Vf=math.sqrt((VI*VI)+(2*a*d));
+
+print(Vf);
